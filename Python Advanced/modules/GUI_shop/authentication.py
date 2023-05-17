c221a76d0ba796e5809ff85815a4e52e4a114044
DimitarTamahkyarov <dimitar.tamahkyarov91@gmail.com>
2023-05-18 00:14:31 +0300

    Update authentication.py

--- Python Advanced/modules/GUI_shop/authentication.py
+++ Python Advanced/modules/GUI_shop/authentication.py
@@ -12,9 +12,10 @@ def render_entry():
         bg="green",      # Background color
         fg="white",       # Font color
         borderwidth=0,
-        width=15,
+        width=11,
         height=2,
-        command=register
+        command=register,
+        font=15
     )
 
     login_btn = Button(
@@ -23,20 +24,45 @@ def render_entry():
         bg="blue",  # Background color
         fg="white",  # Font color
         borderwidth=0,
-        width=15,
+        width=11,
         height=2,
-
+        font=15
     )
 
-    frame.create_window(250, 280, window=register_btn)
+    frame.create_window(250, 270, window=register_btn)
     frame.create_window(250, 320, window=login_btn)
 
 
 def register():
     clean_screen()
 
+    frame.create_text(180, 150, text="First name: ", font=20)
+    frame.create_text(180, 200, text="Last name: ", font=20)
+    frame.create_text(180, 250, text="Username: ", font=20)
+    frame.create_text(180, 300, text="Password: ", font=20)
 
-first_name_box = Entry(root, bd=0)
-last_name_box = Entry(root, bd=0)
-username_box = Entry(root, bd=0)
-password_box = Entry(root, bd=0)
+    frame.create_window(320, 150, window=first_name_box)
+    frame.create_window(320, 200, window=last_name_box)
+    frame.create_window(320, 250, window=username_box)
+    frame.create_window(320, 300, window=password_box)
+
+    registration_btn = Button(
+        root,
+        text="Register",
+        font=15,
+        bg="green",
+        fg="white",
+        command=registration
+    )
+
+    frame.create_window(250, 340, window=registration_btn)
+
+
+def registration():
+    pass
+
+
+first_name_box = Entry(root, bd=0, font=15)
+last_name_box = Entry(root, bd=0, font=15)
+username_box = Entry(root, bd=0, font=15)
+password_box = Entry(root, bd=0, font=15, show="*")
