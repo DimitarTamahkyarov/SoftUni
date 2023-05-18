f9b023ab5473464e3f4b9e00a21eab85be2bc91f
DimitarTamahkyarov <dimitar.tamahkyarov91@gmail.com>
2023-05-19 00:21:54 +0300

    update GUI shop

--- Python Advanced/modules/GUI_shop/authentication.py
+++ Python Advanced/modules/GUI_shop/authentication.py
@@ -1,7 +1,19 @@
+from json import loads, dump
 from tkinter import Button, Entry
+import string
 
 from canvas import root, frame
 from helpers import clean_screen
+
+
+def get_users_data():
+    info_data = []
+
+    with open("db/user_credentials_db.txt", "r") as users_file:
+        for line in users_file:
+            info_data.append(loads(line))
+
+    return info_data
 
 
 def render_entry():
@@ -59,7 +71,119 @@ def register():
 
 
 def registration():
-    pass
+    user_info_dict = {
+        "first_name": first_name_box.get(),
+        "last_name": last_name_box.get(),
+        "username": username_box.get(),
+        "password": password_box.get()
+    }
+
+    if check_registration(user_info_dict):
+        with open("db/user_credentials_db.txt", "a") as users_file:
+            dump(user_info_dict, users_file)
+            users_file.write("\n")
+
+
+def check_registration(dict_info: dict):
+
+    for el, value in dict_info.items():
+        if value == "":
+
+            frame.delete("error")
+
+            frame.create_text(
+                250,
+                380,
+                text="All field must be filled",
+                font=20,
+                fill="red",
+                tag="error"
+            )
+
+            return False
+
+    for char in dict_info["first_name"]:
+
+        if char not in string.ascii_letters:
+
+            frame.delete("error")
+
+            frame.create_text(
+                250,
+                380,
+                text="First name must be only letters",
+                font=20,
+                fill="red",
+                tag="error"
+            )
+
+            return False
+
+    for char in dict_info["last_name"]:
+        if char not in string.ascii_letters:
+            frame.delete("error")
+
+            frame.create_text(
+                250,
+                380,
+                text="Last name must be only letters",
+                font=20,
+                fill="red",
+                tag="error"
+            )
+
+            return False
+
+    if len(dict_info["username"]) < 6:
+
+        frame.delete("error")
+
+        frame.create_text(
+            250,
+            380,
+            text="The username must be more than 5 symbols",
+            font=20,
+            fill="red",
+            tag="error"
+        )
+
+        return False
+
+    elif len(dict_info["username"]) > 16:
+
+        frame.delete("error")
+
+        frame.create_text(
+            250,
+            380,
+            text="The username must be less than 17 symbols",
+            font=20,
+            fill="red",
+            tag="error"
+        )
+
+        return False
+
+    info_data = get_users_data()
+
+    for record in info_data:
+        if record["username"] == dict_info["username"]:
+            frame.delete("error")
+
+            frame.create_text(
+                250,
+                380,
+                text="This user already exist!",
+                font=20,
+                fill="red",
+                tag="error"
+            )
+
+            return False
+
+    frame.delete("error")
+
+    return True
 
 
 first_name_box = Entry(root, bd=0, font=15)
