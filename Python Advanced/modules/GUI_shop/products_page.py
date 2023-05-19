314476277114b8e523504f2a24d6a90ab95429b6
DimitarTamahkyarov <dimitar.tamahkyarov91@gmail.com>
2023-05-19 22:37:47 +0300

    make product page

--- Python Advanced/modules/GUI_shop/products_page.py
+++ Python Advanced/modules/GUI_shop/products_page.py
@@ -1,18 +1,29 @@
 from json import load
-from PIL import Image, ImageTk
+from PIL import ImageTk, Image
 
 from helpers import clean_screen
+from canvas import frame
 
 
 def display_products():
     clean_screen()
+    display_stock()
 
 
-def displey_stock():
+def display_stock():
     with open("db/product_data.json", "r") as stock:
         products_info = load(stock)
 
-    x, y = 100, 50
+    x, y = 250, 20
 
     for product, info in products_info.items():
         item_img = ImageTk.PhotoImage(Image.open(info["image"]))
+        images.append(item_img)
+
+        frame.create_text(x, y, text=product, font=("Comic Sans MS", 15))
+        frame.create_image(x, y + 120, image=item_img,)
+
+        y += 280
+
+
+images = []
