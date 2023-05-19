8c3ce97872657219c00fd60cd1dfcd8051d1ecbf
DimitarTamahkyarov <dimitar.tamahkyarov91@gmail.com>
2023-05-19 23:18:40 +0300

    update GUI shop

--- Python Advanced/modules/GUI_shop/products_page.py
+++ Python Advanced/modules/GUI_shop/products_page.py
@@ -1,8 +1,10 @@
 from json import load
+from tkinter import Button
+
 from PIL import ImageTk, Image
 
 from helpers import clean_screen
-from canvas import frame
+from canvas import frame, root
 
 
 def display_products():
@@ -14,14 +16,33 @@ def display_stock():
     with open("db/product_data.json", "r") as stock:
         products_info = load(stock)
 
-    x, y = 250, 20
+    x, y = 200, 20
 
     for product, info in products_info.items():
         item_img = ImageTk.PhotoImage(Image.open(info["image"]))
         images.append(item_img)
 
         frame.create_text(x, y, text=product, font=("Comic Sans MS", 15))
-        frame.create_image(x, y + 120, image=item_img,)
+        frame.create_image(x, y + 120, image=item_img)
+
+        if info["quantity"] > 0:
+            color = "green"
+            text = f"In stock: {info['quantity']}"
+
+            item_btn = Button(
+                root,
+                text="Buy",
+                bg="green",
+                fg="white",
+                font=15
+            )
+
+            frame.create_window(400, y + 150, window=item_btn)
+        else:
+            color = "red"
+            text = "Out of stock"
+
+        frame.create_text(400, y + 100, text=text, fill=color, font=15)
 
         y += 280
 
