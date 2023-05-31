54ba2954853e8e6eac5adf864ba5e8d32dbbc6e0
DimitarTamahkyarov <dimitar.tamahkyarov91@gmail.com>
2023-05-31 08:46:20 +0300

    Create guild.py

---
+++ OOP/project/guild.py
@@ -0,0 +1,23 @@
+from project.player import Player
+
+
+class Guild:
+    def __init__(self, name: str):
+        self.name = name
+        self.players = []
+
+    def assign_player(self, player: Player):
+        if player.guild == self.name:
+            return f"Player {player.name} is already in the guild."
+        elif player.guild != "Unaffiliated":
+            return f"Player {player.name} is in another guild."
+
+        self.players.append(player)
+        player.guild = self.name
+
+        return f"Welcome player {player.name} to the guild {self.name}"
+
+    def kick_player(self, player_name: str):
+        for player in self.players:
+            if player.name == player_name:
+                
