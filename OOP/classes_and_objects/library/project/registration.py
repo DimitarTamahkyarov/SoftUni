54c56ec7cee9f49399afa49c0802374344bc5071
DimitarTamahkyarov <dimitar.tamahkyarov91@gmail.com>
2023-06-03 22:08:51 +0300

    Update registration.py

--- OOP/classes_and_objects/library/project/registration.py
+++ OOP/classes_and_objects/library/project/registration.py
@@ -32,16 +32,4 @@ class Registration:
         return f"There is no user with id = {user_id}!"
 
 
-user = User(12, 'Peter')
-library = Library()
-registration = Registration()
-registration.add_user(user, library)
-library.books_available.update({'J.K.Rowling': ['The Chamber of Secrets',
-                                                'The Prisoner of Azkaban',
-                                                'The Goblet of Fire',
-                                                'The Order of the Phoenix',
-                                                'The Half-Blood Prince',
-                                                'The Deathly Hallows']})
-library.get_book('J.K.Rowling', 'The Deathly Hallows', 10, user)
-print(user)
 
