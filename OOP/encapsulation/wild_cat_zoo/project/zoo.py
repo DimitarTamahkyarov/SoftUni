c23ee0b6b1e8fcba364950090a42cba41d26f032
DimitarTamahkyarov <dimitar.tamahkyarov91@gmail.com>
2023-06-10 23:07:58 +0300

    update Wild cat zoo

---
+++ OOP/encapsulation/wild_cat_zoo/project/zoo.py
@@ -0,0 +1,9 @@
+class Zoo:
+    def __init__(self, name: str, budget: int, animal_capacity: int, workers_capacity: int):
+        self.name = name
+        self.__budget = budget
+        self.__animal_capacity = animal_capacity
+        self.__workers_capacity = workers_capacity
+        self.animals = []
+        self.workers = []
+
