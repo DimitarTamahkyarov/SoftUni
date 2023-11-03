562162518261d6ecdf5e1d9284c0b3d3f1ecf90a
DimitarTamahkyarov <dimitar.tamahkyarov91@gmail.com>
2023-11-03 19:56:58 +0200

    update

--- ORM/06/main_app/models.py
+++ ORM/06/main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models import CASCADE
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=40)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=40)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    author = models.ForeignKey('Author', on_delete=CASCADE)
