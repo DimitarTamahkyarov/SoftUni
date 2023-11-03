2195f6616daf59d9280e9a27a09de005b43e9430
DimitarTamahkyarov <dimitar.tamahkyarov91@gmail.com>
2023-11-03 21:12:34 +0200

    update

--- ORM/06/caller.py
+++ ORM/06/caller.py
@@ -1,10 +1,49 @@
 import os
 import django
 
+
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+from main_app.models import Author, Book, Artist, Song
+
 
 # Create queries within functions
+def show_all_authors_with_their_books():
+    result = []
+    authors = Author.objects.all().order_by('id')
+
+    for author in authors:
+        books = author.book_set.all()
+        if books:
+            result.append(f'{author.name} has written - {", ".join(str(b) for b in books)}!')
+
+    return '\n'.join(result)
+
+
+def delete_all_authors_without_books():
+    Author.objects.filter(book__isnull=True).delete()
+
+
+#
+# # Display authors and their books
+# authors_with_books = show_all_authors_with_their_books()
+# print(authors_with_books)
+
+# Delete authors without books
+# delete_all_authors_without_books()
+# print(Author.objects.count())
+
+def add_song_to_artist(artist_name: str, song_title: str):
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+
+    song.artists.add(artist)
+
+
+def get_songs_by_artist(artist_name: str):
+    artist = Artist.objects.get(name=artist_name)
+    songs = artist.
+
--- ORM/06/main_app/models.py
+++ ORM/06/main_app/models.py
@@ -9,4 +9,16 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=40)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    author = models.ForeignKey('Author', on_delete=CASCADE)
+    author = models.ForeignKey(Author, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    songs = models.ManyToManyField(Song, related_name='artists')
